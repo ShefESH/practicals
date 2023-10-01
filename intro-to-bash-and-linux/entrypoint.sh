e8d2b0b6f8061e47ea117de84f2298bd1c7ddb67
Atri Hegde <iamatrihegde@outlook.com>
2023-10-01 16:03:53 +0100

    feat: bash and linux ready

---
+++ intro-to-bash-and-linux/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/sbin/sshd -D
+while true; do sleep 1; done
--- intro-to-bash-and-linux/init.sh
+++ intro-to-bash-and-linux/init.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-for i in {1..100}; do \
-        useradd -m -s /bin/bash -p "$(openssl passwd -1 user${i})" user$i; \
-done
-
-while true; do sleep 1; done
+mkdir /run/sshd
+# Make sure script doesn't run twice
+if ! grep -q user80 "/etc/passwd"; then
+    # Create users
+    for num in $(seq 1 80)
+    do
+        password=$(echo 'SESH_LINUX' | openssl passwd -6 -stdin)
+        username=user$num
+        useradd -m -s /bin/bash $username --password $password
+        # all actions as user/group
+        sudo runuser "$username" -g $username
+        # Add files and folders for each user
+        echo "Good job finding this hidden file!" > /home/$username/.hidden.txt
+        echo "Tomatoes" > /home/$username/fruit.txt
+        echo "- Grapes\n- Oranges\n- Peaches" > /home/$username/rename.txt
+        mkdir /home/$username/tree && echo "Just a leaf" > /home/$username/tree/leaf.txt
+        # Use root user/group
+        sudo runuser "root" -g root
+        # Create a file as root
+        echo "high permissions" > /home/$username/perms.txt
+    done
+fi
